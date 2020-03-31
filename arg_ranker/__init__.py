c3c1b8022068acfe1996a4ead8d7f54a3369344d
caozhichongchong <caozhichongchong@gmail.com>
2020-03-31 13:48:19 -0400

    update ARG risk list

--- arg_ranker/__init__.py
+++ arg_ranker/__init__.py
@@ -1,3 +1,3 @@
 import arg_ranker
 
-__version__ = "2.0"
+__version__ = "2.1"
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="arg_ranker",
     packages=['arg_ranker'],
-    version="2.0",
+    version="2.1",
     description="Ranking the risk of antibiotic resistance for metagenomes",
     author='An-Ni Zhang',
     author_email='anniz44@mit.edu',
